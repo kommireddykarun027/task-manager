ec42a590754bc447c2a64352d340c4305d650717
kommireddykarun027 <kommireddykarun027@gmail.com>
2025-11-20 19:21:26 +0530

    Initial project setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Task Manager
+
+Simple Task Manager built with HTML/CSS/JavaScript.  
+Steps to run:
+1. Open `index.html` in a browser.
---
+++ app.js
@@ -0,0 +1,5 @@
+// Starter JS - will expand later
+document.getElementById('tasks').textContent = 'No tasks yet. Click "Add Task" to create one.';
+document.getElementById('openAddBtn').addEventListener('click', () => {
+  alert('We will add a form here in the next step.');
+});
